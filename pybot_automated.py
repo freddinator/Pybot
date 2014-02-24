65e28e11786dce2ec8dda71ce6a19e006f2990bc
SiteOctopus <f.heppell@gmail.com>
2014-02-24 15:32:19 +0000

    Merged Changes from old repo
    Not a fork, so couldn't pull request

--- pybot_automated.py
+++ pybot_automated.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 
+debug = true
+
 ##setup GPIO - this has to be done somewhere in the code
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH)
@@ -12,70 +14,86 @@ GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(10, GPIO.IN)
 GPIO.setwarnings(False)
 
-## check killswitch and print
-killswitchstatus = GPIO.input(10)
-print(killswitchstatus)
 
-## functions - these functions actually make the hardware respond on a GPIO level
 
-def forward():
-	GPIO.output(11, GPIO.HIGH)
-    GPIO.output(12, GPIO.HIGH)
+
+## Stop code
+
+def stop():
+    GPIO.output(11, GPIO.LOW)
+    GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.LOW)
-	GPIO.output(16, GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW)
     GPIO.output(18, GPIO.LOW)
     return
 
-def reverse():
-	GPIO.output(11, GPIO.HIGH)
+## check killswitch and print
+def killSwitch():
+    killswitchstatus = GPIO.input(10)
+    if killswitchstatus == 1:
+        print("Kill Switch Activated")
+        stop()
+    return
+
+## functions - these functions actually make the hardware respond on a GPIO level
+
+
+def forward(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(16, GPIO.HIGH)
+    GPIO.output(18, GPIO.LOW)
+    killSwitch()
+    time.sleep(time)
+    return
+
+def reverse(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.HIGH)
     GPIO.output(16, GPIO.LOW)
     GPIO.output(18, GPIO.HIGH)
+    killSwitch()
+    time.sleep(time)
     return
     
-def left():
-	GPIO.output(11, GPIO.HIGH)
+def left(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.HIGH)
     GPIO.output(16, GPIO.HIGH)
     GPIO.output(18, GPIO.LOW)
-	return
+    killSwitch()
+    time.sleep(time)
+    return
 	
 	
-def right():
-	GPIO.output(11, GPIO.HIGH)
+def right(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.HIGH)
     GPIO.output(13, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
     GPIO.output(18, GPIO.HIGH)
+    time.sleep(time)
     return
     
-def stop():
-	GPIO.output(11, GPIO.LOW)
-    GPIO.output(12, GPIO.LOW)
-    GPIO.output(13, GPIO.LOW)
-    GPIO.output(16, GPIO.LOW)
-    GPIO.output(18, GPIO.LOW)
-    return
+
 	
 ##main loop
 
-while  1:
-		#Go Forward for 1 sec
-		forward()
-		time.sleep(1)
+while 1:
+    #Go Forward for 1 sec
+    forward(1)
 		
-		#Turn Right 90 degrees
-		left()
-		time.sleep(1)
+    #Turn Right 90 degrees
+    left(1)
 		
-		#add more movement here
-		break
+    #add more movement here
+    break
         
-GPIO.cleanup()        
-    
-
-
-
-
+GPIO.cleanup()
